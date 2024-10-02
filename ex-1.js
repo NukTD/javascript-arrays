96542c7140fb5ce9dc61dccb5e2a294d182f762f
TheYami <luffy90san@gmail.com>
2024-10-02 13:13:46 +0700

    ex1

--- ex-1.js
+++ ex-1.js
@@ -6,10 +6,22 @@ let fourthColor;
 let fifthColor;
 let tenthColor;
 
+//ลำดับของ Value ที่อยู่ใน Array
 blueColor = colors[3];
 fourthColor = colors[4];
 fifthColor = colors[5];
 tenthColor = colors[10];
+
+console.log(blueColor);
+console.log(fourthColor);
+console.log(fifthColor);
+console.log(tenthColor);
+
+//Index ของ Value ใน Array
+blueColor = colors[2];
+fourthColor = colors[3];
+fifthColor = colors[4];
+tenthColor = colors[9];
 
 console.log(blueColor);
 console.log(fourthColor);
